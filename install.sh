885c7d258712bbc75d84e43a34d394f0aa188c95
Robert Buren <robert@buren.se>
2017-12-11 20:46:32 +0100

    now correctly uses https clone url in install script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd /home/ec2-user
 sudo yum update -y
 sudo curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
 sudo yum install git nodejs -y
-git clone git@github.com:cygni/cygni-competence-aws1-ec2album.git ec2-album
+git clone https://github.com/cygni/cygni-competence-aws1-ec2album.git ec2-album
 cd ec2-album
 sed -i 's/examplebucket/ACTUALBUCKETNAME/g' index.js
 npm install
